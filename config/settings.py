06da15211790c940952baa63e18188ce66131f33
Lucas Pollo <lucasrefinskipollo@gmail.com>
2026-04-19 22:15:54 -0300

    feat: configuraçoes de base postgres e cnes

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,8 +76,20 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME', default='app_db'),
+        'USER': config('DB_USER', default='postgres'),
+        'PASSWORD': config('DB_PASSWORD', default='123456'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
+    },
+    'cnes': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('CNES_DB_NAME', default='cnes'),
+        'USER': config('CNES_DB_USER', default='postgres'),
+        'PASSWORD': config('CNES_DB_PASSWORD', default='123456'),
+        'HOST': config('CNES_DB_HOST', default='localhost'),
+        'PORT': config('CNES_DB_PORT', default='5432'),
     }
 }
 
